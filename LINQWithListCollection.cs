ab0a8004e76b5341b8426353e9024881620eec7a
Gabriel Vega <gvega001@gmail.com>
2021-03-10 17:00:23 -0800

    Interate threw array

--- LINQWithListCollection.cs
+++ LINQWithListCollection.cs
@@ -8,7 +8,20 @@ namespace Array
 {
     class LINQWithListCollection
     {
+        static void Main(string[] args)
+        {
+            //populate list of strings
+            var groceryListItems = new List<string>();
+            groceryListItems.Add("Milk");
+            groceryListItems.Add("cereal");
+            groceryListItems.Add("Butter");
+            groceryListItems.Add("oranges");
 
+            for (int counter = 0; counter < groceryListItems.Count; counter++)
+            {
+                Console.WriteLine($"{counter}{groceryListItems[counter]}");
+            }
 
+        }
     }
 }
